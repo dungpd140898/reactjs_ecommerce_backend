d8d143bd9bd36b08d2227699b6e459f211ab25d3
dungpd140898 <dungpd1998@gmail.com>
2023-07-27 00:10:10 +0700

    upload website

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Welcome')
+});
+app.listen(3001, () => {
+    console.log("listening on port 3001");
+});
